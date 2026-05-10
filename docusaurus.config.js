7483828a429c2234566417f02fbbcb4582d17982
saheen.palayi <emspsaheen@gmail.com>
2026-05-10 18:13:39 +0530

    updaated the base url again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://saheenpalayi.com',
+  url: 'http://projects.saheenpalayi.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ProjectsDoc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
